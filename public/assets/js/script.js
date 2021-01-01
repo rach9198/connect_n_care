592a9bf94943853166a315764cd6272c29f3098a
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2021-01-01 13:52:36 +0530

    inital setup with firebase

---
+++ public/assets/js/script.js
@@ -0,0 +1,12 @@
+// modal
+const loginButton = document.querySelector('#login-button');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
+
+loginButton.addEventListener('click', () => {
+  modal.classList.add('is-active');
+});
+
+modalBg.addEventListener('click', () => {
+  modal.classList.remove('is-active');
+});
